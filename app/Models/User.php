e4b3c0cb98f13998461487b284e29604c6e10273
Jessica Turner <jessica.turner@continued.com>
2026-09-18 13:12:04 -0400

    Design Your Model Layer

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,34 +1,42 @@
 <?php
 
-declare(strict_types=1);
+    declare(strict_types=1);
 
-namespace App\Models;
+    namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Database\Factories\UserFactory;
-use Illuminate\Database\Eloquent\Attributes\Fillable;
-use Illuminate\Database\Eloquent\Attributes\Hidden;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+    // use Illuminate\Contracts\Auth\MustVerifyEmail;
+    use Database\Factories\UserFactory;
+    use Illuminate\Database\Eloquent\Attributes\Fillable;
+    use Illuminate\Database\Eloquent\Attributes\Hidden;
+    use Illuminate\Database\Eloquent\Factories\HasFactory;
+    use Illuminate\Database\Eloquent\Relations\HasMany;
+    use Illuminate\Foundation\Auth\User as Authenticatable;
+    use Illuminate\Notifications\Notifiable;
+    use Override;
 
-#[Fillable(['name', 'email', 'password'])]
-#[Hidden(['password', 'remember_token'])]
-class User extends Authenticatable
-{
-    /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
+    #[Fillable(['name', 'email', 'password'])]
+    #[Hidden(['password', 'remember_token'])]
+    class User extends Authenticatable
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        /** @use HasFactory<UserFactory> */
+        use HasFactory, Notifiable;
+
+        public function idea(): HasMany
+        {
+            return $this->hasMany(Idea::class);
+        }
+
+        /**
+         * Get the attributes that should be cast.
+         *
+         * @return array<string, string>
+         */
+        #[Override]
+        protected function casts(): array
+        {
+            return [
+                'email_verified_at' => 'datetime',
+                'password' => 'hashed',
+            ];
+        }
     }
-}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,26 +1,31 @@
 <?php
 
-declare(strict_types=1);
+    declare(strict_types=1);
 
-namespace App\Providers;
+    namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+    use Illuminate\Database\Eloquent\Model;
+    use Illuminate\Support\ServiceProvider;
+    use Override;
 
-class AppServiceProvider extends ServiceProvider
-{
-    /**
-     * Register any application services.
-     */
-    public function register(): void
+    class AppServiceProvider extends ServiceProvider
     {
-        //
-    }
+        /**
+         * Register any application services.
+         */
+        #[Override]
+        public function register(): void
+        {
+            //
+        }
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        //
+        /**
+         * Bootstrap any application services.
+         */
+        public function boot(): void
+        {
+            Model::unguard();
+            Model::shouldBeStrict();
+            Model::automaticallyEagerLoadRelationships();
+        }
     }
-}
